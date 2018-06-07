f2f8093b460fb4961fe177fbb6e054d35b1da055
Khalil Stemmler <metroidman12@gmail.com>
2018-06-07 13:28:33 -0400

    Add netlify to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,44 @@
-// module.exports = {
-//   siteMetadata: {
-//     title: 'Gatsby Default Starter',
-//   },
-//   plugins: [
-    
-//     {
-//       resolve: `gatsby-plugin-typography`,
-//       options: {
-//         pathToConfigModule: `src/utils/typography.js`,
-//       },
-//     }
-//   ],
-// }
-
-
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Univjobs',
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-react-helmet'
-    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-};
+}
