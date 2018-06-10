d1a92ba747fd94776882fa2595084629542262d7
Charles Javelona <charlesjavelona@gmail.com>
2018-06-10 17:54:37 -0400

    Job header details done

--- src/pages/posting/index.js
+++ src/pages/posting/index.js
@@ -2,16 +2,54 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import ShortHero from '../../components/ShortHero'
+import job from '../../styles/JobPosting.module.css'
 
-import s from '../../img/merchant.jpg'
+import haltech from '../../img/haltech.png'
+import briefcase from '../../img/briefcase.svg'
+import money from '../../img/money.svg'
+import place from '../../img/place.svg'
 
 
-export default () =>
+
+
+const JobHeader = (props) => {
+    return(
+        <section className={job.jobHeaderContainer}>
+        <div className={job.centeredJobContainer}>
+                <div className={job.companyBrandImageContainer}>
+                    <img className={job.companyBrandImage} src={haltech}/>
+                </div>
+                <h3 className={job.jobTitle}>Haltech</h3>
+                <span style={{marginLeft: '8%'}} className={job.jobHeaderInfo}>
+                    <img className={job.icons}src={place}/>
+                </span>
+                <p className={job.iconText}>Toronto, ON</p>
+                <span className={job.jobHeaderInfo}>
+                    <img className={job.icons}src={briefcase}/>
+                </span>
+                <p className={job.iconText}>Entry-level</p>
+                <span className={job.jobHeaderInfo}>
+                    <img className={job.icons}src={money}/>
+                </span>
+                <p className={job.iconText}>$15/h</p>
+
+                <button className={job.applyHeaderButton}>APPLY NOW</button>
+
+        </div>
+      </section>
+
+    )
+
+}
+
+
+export default () => 
   <div>
       <ShortHero
          heroTitle= 'Job Test'
          heroSubTitle= ''
-         image={s}
+         image=""
       />
-   
+      <JobHeader/>
+     
   </div>
