14014291c1c673a4452e576ea8051d5ab2f71ac0
Khalil Stemmler <metroidman12@gmail.com>
2018-06-11 11:06:07 -0400

    Update admin.yml

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,6 @@
 import CMS from 'netlify-cms'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
