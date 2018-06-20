b0c3d3c71ef62ccd591a8b7fb302cc5eb7bba246
Julio Tain Sueiras <juliosueiras@gmail.com>
2018-06-20 19:02:13 -0400

    Added function for posting

---
+++ func_src/posting.js
@@ -0,0 +1,47 @@
+'use strict';
+var https = require('https');
+var axios = require('axios');
+var jsdom = require('jsdom');
+var striptags = require("striptags");
+
+
+exports.handler = function(event, context, callback) {
+	//exports.handler = async function(event, context, callback) {
+
+	var splits = event.path.split("/");
+	var job_id = splits[splits.length-1];
+
+	axios.get(`https://api.univjobs.ca/api/v1/public/job/${job_id}`).then((r) => {
+
+		axios.get('http://app.univjobs.ca').then((t) => {
+
+			var test3 = new jsdom.JSDOM(t.data);
+
+			var job = r.data.job;
+
+			var title = `${job.title} @ ${job.Employer.company_name}`;
+			var url = `http://app.univjobs.ca/posting/${job_id}`;
+			var description = striptags(job.compensation);
+			var image = job.Employer.logo_url;
+
+			test3.window.document.querySelector("[property='og:title']").setAttribute("content",title);
+			test3.window.document.querySelector("[property='og:url']").setAttribute("content",url);
+			test3.window.document.querySelector("[property='og:description']").setAttribute("content",description);
+			test3.window.document.querySelector("[property='og:image']").setAttribute("content",image);
+			test3.window.document.querySelector("[property='og:type']").setAttribute("content","website");
+
+			var response = {
+				statusCode: 200,
+				headers: {
+					"Content-Type": "text/html"
+				},
+				body: test3.window.document.documentElement.outerHTML
+			}
+			callback(null,response)
+		}).catch((e) => {
+			callback(e)
+		});
+	}).catch((e) => {
+		callback(e)
+	});
+	};
