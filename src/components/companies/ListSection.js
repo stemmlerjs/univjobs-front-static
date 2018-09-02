11f8b6ec93399fcaebb02cc8cdc49587d9b19aeb
Charles Javelona <charlesjavelona@gmail.com>
2018-09-02 17:20:45 -0400

    Updated

--- src/components/companies/ListSection.js
+++ src/components/companies/ListSection.js
@@ -19,16 +19,29 @@ class ListSection extends React.Component {
     super();
   }
 
+  hasList (boolean) {
+
+    if(boolean) {
+      return <div className="list-section">
+       {
+         boolean.map((item, i) => (
+          <ListItem key={i} text={item}/>
+        ))
+       }
+      </div>
+    } else {
+      return <div/>
+     
+    }
+    
+  }
+
+
+
   render() {
     const { list } = this.props;
     return (
-      <div className="list-section">
-        {
-          list.map((item, i) => (
-            <ListItem key={i} text={item}/>
-          ))
-        }
-      </div>
+          this.hasList(list)
     )
   }
 }
