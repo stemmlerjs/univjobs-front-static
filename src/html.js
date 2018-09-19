31f9d953582996d0ab26dc020de685d95161a1bb
Khalil Stemmler <metroidman12@gmail.com>
2018-09-19 12:58:00 -0400

    Try something different

--- src/html.js
+++ src/html.js
@@ -41,21 +41,6 @@ module.exports = class HTML extends React.Component {
           />
           {this.props.postBodyComponents}
         </body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '336246053615452');
-              fbq('track', 'PageView');`
-          }}
-        />
         <noscript>
           <img
             height="1"
