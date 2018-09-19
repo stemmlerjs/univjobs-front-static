6150fed7e4cfaad86e2b5782fbed8dd3df6c0df6
Khalil Stemmler <metroidman12@gmail.com>
2018-09-19 13:07:14 -0400

    Add facebook pixel plugin instead.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,93 +9,104 @@ require("dotenv").config({
   path: `.env.${activeEnv}`,
 });
 
+const plugins = [
+  {
+    resolve: "gatsby-univjobs-api",
+    options: {
+      url: process.env.UNIVJOBS_DATASOURCE_URL,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-101892655-1",
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/admin/**", "/do-not-track/me/too/"],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./src/favicon.png",
+      injectHTML: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }
+  },
+  {
+    resolve: `gatsby-plugin-sitemap`
+  },
+  `gatsby-plugin-compression`,
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography.js`,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-postcss-sass`,
+    options: {
+      postCssPlugins: [require('autoprefixer')()],
+      precision: 8, // SASS default: 5
+    },
+  },
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-sass',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/pages`,
+      name: 'pages',
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/img`,
+      name: 'images',
+    },
+  },
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [],
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-netlify-cms',
+    options: {
+      modulePath: `${__dirname}/src/cms/cms.js`,
+    },
+  },
+  'gatsby-plugin-netlify', // make sure to keep it last in the array
+]
+
+if (activeEnv === "production") {
+  plugins.push({
+    resolve: `gatsby-plugin-facebook-pixel`,
+    options: {
+      pixelId: '336246053615452',
+    },
+  })
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Univjobs - Simplifying hiring post-secondary students',
     siteUrl: `https://univjobs.ca`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-univjobs-api",
-      options: {
-        url: process.env.UNIVJOBS_DATASOURCE_URL,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-101892655-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/admin/**", "/do-not-track/me/too/"],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/favicon.png",
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
-    {
-      resolve: `gatsby-plugin-sitemap`
-    },
-    `gatsby-plugin-compression`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-postcss-sass`,
-      options: {
-        postCssPlugins: [require('autoprefixer')()],
-        precision: 8, // SASS default: 5
-      },
-    },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
+  plugins: plugins
 }
--- src/config/index.js
+++ src/config/index.js
@@ -17,18 +17,5 @@ console.log(process.env.BRANCH)
 
 if (typeof window !== "undefined") {
   window.config = config;
-
-  if (process.env.CURRENT_ENV === "production") {
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '336246053615452');
-    fbq('track', 'PageView');
-  }
 }
 
