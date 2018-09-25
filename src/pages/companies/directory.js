58dba040a4d878cef37a21f2481da3c240162892
Khalil Stemmler <metroidman12@gmail.com>
2018-09-25 07:48:24 -0400

    Remove comments breaking uglify build.

--- src/pages/companies/directory.js
+++ src/pages/companies/directory.js
@@ -11,62 +11,6 @@ import CallToAction from '../../components/CallToAction'
 import config from '../../config'
 import '../../styles/Directory/Directory.sass'
 
-/**
- * https://medium.com/@morgannegagne/google-maps-with-react-951c12b723ad
- * https://tomchentw.github.io/react-google-maps/
- * https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/
- */
-
-// const companies = [
-//   {
-//     companyName: 'Rover',
-//     address: '1428 White Oaks Blvd',
-//     industry: {
-//       value: 5,
-//       label: 'Service',
-//     },
-//     companySize: { value: 1, label: '1 - 9' },
-//     about: `Dog-walking and pet-sitting at your own convenience. 
-//       We help blah blah blah so o lklf lorem ipsum, etc.`,
-//     featured: true,
-//     logoUrl:
-//       'https://freebiemom.r.worldssl.net/wp-content/uploads/2018/08/Rover-logo.jpg',
-//     jobs: [
-//       {
-//         title: 'Software Developer - Co-op',
-//         slug: '/software-developer-co-op-12343nkd',
-//       },
-//       { title: 'Sales associate - Brampton', slug: '/salesassociate' },
-//       { title: 'Entry level Dev-ops', slug: '/entry-level-dev-ops' },
-//     ],
-//     exploreSlug: '/companies/rover',
-//     position: {
-//       lat: 43.65077,
-//       lng: -79.37581,
-//     },
-//     hiring: true,
-//   },
-//   {
-//     jobs: [],
-//     companyName: 'Wealthsimple',
-//     address: '1524 Front Avenue, Toronto ON',
-//     companySize: { value: 2, label: '10 - 99' },
-//     industry: {
-//       value: 6,
-//       label: 'Finance',
-//     },
-//     about: `We help you use your money for smart investments.`,
-//     featured: false,
-//     logoUrl:
-//       'https://images.startupopenhouse.com/thumbnail?colorspace=srgb&url=https://s3.amazonaws.com/soh-pwa-files-production/img/874e3c45-47dd-4ee6-a2ce-f9867d43d29c-1653e55f118.jpeg&width=310',
-//     position: {
-//       lat: 43.64491,
-//       lng: -79.41233,
-//     },
-//     hiring: false,
-//   },
-// ]
-
 // Coordinates for Union Station in Toronto. A backup
 // in case we can't get the coordinates for this current
 // user.
