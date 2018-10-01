97af261b33b2a4eb5be5bebfc63c07224cb818ed
Khalil Stemmler <metroidman12@gmail.com>
2018-09-30 23:40:38 -0400

    NEW FUNCTION.

--- func_src/posting.js
+++ func_src/posting.js
@@ -1,63 +1,100 @@
 'use strict';
-var https = require('https');
-var axios = require('axios');
-var jsdom = require('jsdom');
-var striptags = require("striptags");
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request')
 
-
-exports.handler = function(event, context, callback) {
-	//exports.handler = async function(event, context, callback) {
-	var setting = {
-		"staging": {
-			"api": "https://api.01-staging.univjobs.ca",
-			"app": "https://app.01-staging.univjobs.ca",
-		},
-		"prod": {
-			"api": "https://api.univjobs.ca",
-			"app": "https://app.univjobs.ca",
-		}
+// Configure the environment settings
+const setting = {
+	"staging": {
+		"api": "https://api.01-staging.univjobs.ca",
+		"app": "https://app.01-staging.univjobs.ca",
+	},
+	"prod": {
+		"api": "https://api.univjobs.ca",
+		"app": "https://app.univjobs.ca",
 	}
+}
 
-	var splits = event.path.split("/");
-	var job_id = splits[splits.length-1];
-	var env = splits[splits.length-3];
+const getBaseUrl = (req) => {
+	const baseUrl = req.url.split("/")[2];
+	if (baseUrl === "app.01-staging.univjobs.ca") {
+		return setting.staging.app;
+	}
+	return setting.prod.app;
+}
 
-	console.log(splits);
-	console.log(env);
-	var setting = (env == "staging") ? setting.staging : setting.prod;
-	console.log(JSON.stringify(setting));
+app.use(require('prerender-node')
+.set('prerenderToken', 'Ecl3kKs6Lg7ZdBcbaKTF'))
+// .set('forwardHeaders', true);
 
-	axios.get(`${setting.api}/api/v1/public/job/${job_id}`).then((r) => {
+const router = express.Router();
+app.get('/posting/:jobId', (req, res) => request({
+	url: `${getBaseUrl(req)}/posting/${req.params.jobId}`,
+	method: 'GET'
+}).pipe(res));
 
-		axios.get(setting.app).then((t) => {
 
-			var test3 = new jsdom.JSDOM(t.data);
+app.use(bodyParser.json());
+app.use('/.netlify/functions/posting', router);  // path must route to lambda
 
-			var job = r.data.job;
+module.exports = app;
+module.exports.handler = serverless(app);
 
-			var title = `${job.title} @ ${job.Employer.company_name}`;
-			var url = `${setting.app}/posting/${job_id}`;
-			var description = striptags(job.compensation);
-			var image = job.Employer.logo_url;
+// exports.handler = function(event, context, callback) {
+	
+// 	const setting = {
+// 		"staging": {
+// 			"api": "https://api.01-staging.univjobs.ca",
+// 			"app": "https://app.01-staging.univjobs.ca",
+// 		},
+// 		"prod": {
+// 			"api": "https://api.univjobs.ca",
+// 			"app": "https://app.univjobs.ca",
+// 		}
+// 	}
 
-			test3.window.document.querySelector("[property='og:title']").setAttribute("content",title);
-			test3.window.document.querySelector("[property='og:url']").setAttribute("content",url);
-			test3.window.document.querySelector("[property='og:description']").setAttribute("content",description);
-			test3.window.document.querySelector("[property='og:image']").setAttribute("content",image);
-			test3.window.document.querySelector("[property='og:type']").setAttribute("content","website");
+	// var splits = event.path.split("/");
+	// var job_id = splits[splits.length-1];
+	// var env = splits[splits.length-3];
 
-			var response = {
-				statusCode: 200,
-				headers: {
-					"Content-Type": "text/html"
-				},
-				body: test3.window.document.documentElement.outerHTML
-			}
-			callback(null,response)
-		}).catch((e) => {
-			callback(e)
-		});
-	}).catch((e) => {
-		callback(e)
-	});
-	};
+	// // console.log(splits);
+	// // console.log(env);
+	// var setting = (env == "staging") ? setting.staging : setting.prod;
+	// console.log(JSON.stringify(setting));
+
+// 	axios.get(`${setting.api}/api/v1/public/job/${job_id}`).then((r) => {
+
+// 		axios.get(setting.app).then((t) => {
+
+// 			var test3 = new jsdom.JSDOM(t.data);
+
+// 			var job = r.data.job;
+
+// 			var title = `${job.title} @ ${job.Employer.company_name}`;
+// 			var url = `${setting.app}/posting/${job_id}`;
+// 			var description = striptags(job.compensation);
+// 			var image = job.Employer.logo_url;
+
+// 			test3.window.document.querySelector("[property='og:title']").setAttribute("content",title);
+// 			test3.window.document.querySelector("[property='og:url']").setAttribute("content",url);
+// 			test3.window.document.querySelector("[property='og:description']").setAttribute("content",description);
+// 			test3.window.document.querySelector("[property='og:image']").setAttribute("content",image);
+// 			test3.window.document.querySelector("[property='og:type']").setAttribute("content","website");
+
+// 			var response = {
+// 				statusCode: 200,
+// 				headers: {
+// 					"Content-Type": "text/html"
+// 				},
+// 				body: test3.window.document.documentElement.outerHTML
+// 			}
+// 			callback(null,response)
+// 		}).catch((e) => {
+// 			callback(e)
+// 		});
+// 	}).catch((e) => {
+// 		callback(e)
+// 	});
+// 	};
