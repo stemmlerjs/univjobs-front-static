8a32cfdfecde690765a609b30ece97d15cd95ff5
Khalil Stemmler <metroidman12@gmail.com>
2018-11-29 09:22:23 -0500

    Update Twitter card view.

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -118,6 +118,7 @@ const SEO = ({ postData, postImage, isBlogPost }) => {
       {/* Twitter Card tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={config.twitter} />
+      <meta name="twitter:site" content={config.twitter} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ let config = {
   description: 'A place for students and recent grads to find jobs to earn cash and launch their careers.',
   logo: '/img/u.png',
   image: '/img/u.png',
-  twitter: 'https://twitter.com/univjobsapp',
+  twitter: '@univjobsapp',
   fbAppID: '1897052653882765',
   url: process.env.GATSBY_STATIC_URL ? process.env.GATSBY_STATIC_URL : 'https://01-staging.univjobs.ca/',
   appUrl: process.env.GATSBY_APP_URL ? process.env.GATSBY_APP_URL : 'https://app.01-staging.univjobs.ca/',
