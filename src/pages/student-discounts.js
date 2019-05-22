e90b58bb4cf852c551f4058d411d5b8cfc53cd70
Khalil Stemmler <metroidman12@gmail.com>
2019-05-22 14:03:28 -0400

    Update server rendered url.

--- src/pages/student-discounts.js
+++ src/pages/student-discounts.js
@@ -89,6 +89,7 @@ class StudentDiscounts extends React.Component {
 
   getUrl () {
     if (typeof window !== "undefined") {
+      //
       return window.location.href
     } else {
       return ""
