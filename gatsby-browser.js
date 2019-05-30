de8f8ebec3e0cfd792683d94105e0641aa33ae9b
Khalil Stemmler <metroidman12@gmail.com>
2019-05-30 16:52:20 -0400

    Add cookie code.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -232,3 +232,24 @@ exports.onClientEntry = () => {
     require('./gatsby-prod-scripts')
   }
 }
+
+function setCookie (name, value) {
+  document.cookie = name + "=" + (value || "") + ";domain=.univjobs.ca";
+}
+
+
+function getCookie (name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+
+function eraseCookie (name) {   
+  document.cookie = name+'=; Max-Age=-99999999;';  
+}
